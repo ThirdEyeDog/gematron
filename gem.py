493cf8d5504301db82c52bc785c76b04c4a769d6
ThirdEyeDog <140479281+ThirdEyeDog@users.noreply.github.com>
2023-08-16 19:46:04 -0400

    fixed web search bug, added sentiment

--- gem.py
+++ gem.py
@@ -1,23 +1,26 @@
-# V0.0.0.4 TAROT FLATENED
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
-from langchain.memory import ConversationEntityMemory
+#v.0.0.0.6 GEMATRON
+
+# run on a jupypter notebook to have a better experience
+# install list: !pip install  transformers torch secrets matplotlib numpy langchain playwright 
+
+from transformers import RobertaTokenizer, RobertaForSequenceClassification
+import torch
+from torch.nn.functional import softmax
+import secrets
+import matplotlib.pyplot as plt
+import numpy as np
+import unittest
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from playwright.async_api import async_playwright
-from IPython.display import Image, display
-from PIL import Image as PILImage
 from langchain.llms import OpenAI
 from bs4 import BeautifulSoup
 import urllib.parse
 import asyncio
 import hashlib
 import urllib
-import random
 import torch
 import re
 import os
-
-
 import time
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -26,93 +29,88 @@ model_name = "gpt2-medium"
 gpt2_tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 gpt2_model = GPT2LMHeadModel.from_pretrained(model_name)
 
-#v.0.0.0.4
-# TAROT
+# Load pre-trained model and tokenizer
+tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+model = RobertaForSequenceClassification.from_pretrained('roberta-base', num_labels=2) # Assuming binary sentiment
 
 
 
 
 
+
+RULE = {(1, 1, 1): 0, (1, 1, 0): 0, (1, 0, 1): 0, (1, 0, 0): 1, (0, 1, 1): 1, (0, 1, 0): 1, (0, 0, 1): 1, (0, 0, 0): 0}
 
 def rule_30(current_generation, generations=7):
     key_binary = ""
+    # We'll save all generations in a list for the visualization
+    all_generations = [current_generation]
     for _ in range(generations):
         next_generation = []
-        # ASCII visualization of the current generation
-        print(''.join('#' if cell == 1 else '.' for cell in current_generation))
-        time.sleep(0.3) # Delay for animation effect
         for i in range(len(current_generation)):
             left = current_generation[i - 1] if i > 0 else 0
             center = current_generation[i]
             right = current_generation[(i + 1) % len(current_generation)]
-            next_cell = (left, center, right)
-            rule = {(1, 1, 1): 0, (1, 1, 0): 0, (1, 0, 1): 0, (1, 0, 0): 1, (0, 1, 1): 1, (0, 1, 0): 1, (0, 0, 1): 1, (0, 0, 0): 0}
-            next_generation.append(rule[next_cell])
+            next_generation.append(RULE[(left, center, right)])
         key_binary += ''.join(map(str, next_generation))
+        all_generations.append(next_generation) # Save the generation
         current_generation = next_generation
-    key_hex = hex(int(key_binary[:28], 2))[2:].zfill(7) # Using only the first 28 bits to create a 7-character key
-    return key_hex
 
+    # Visualization of the automaton
+    plt.imshow(np.array(all_generations), cmap="binary", interpolation="none")
+    plt.show()
+
+    key_hex = hex(int(key_binary[:28], 2))[2:].zfill(7)
+    return key_hex
 
 
 # define search function
 
-async def fetch_search_results(query):
+
+
+# 
+async def fetch_search_results(query, max_results=20):
+    content = ""
     async with async_playwright() as p:
         browser = await p.chromium.launch()
         page = await browser.new_page()
-        search_url = f"https://www.bing.com/search?q={urllib.parse.quote(query)}"
+        search_url = f"https://www.bing.com/search?q={urllib.parse.quote(query)}&PC=U316&FORM=CHROMN"
         await page.goto(search_url)
         await page.wait_for_selector(".b_algo")
 
-        # Get the link of the first search result
-        first_link_element = await page.query_selector(".b_algo h2 a")
-        
-        # If there's no link found, we simply return an empty string
-        if not first_link_element:
-            await browser.close()
-            return ""
+        # Loop through the search results
+        for i in range(max_results):
+            link_element = await page.query_selector(f".b_algo:nth-child({i + 1}) h2 a")
 
-        first_link = await first_link_element.get_attribute("href")
-        
-        # Navigate to the first link
-        await page.goto(first_link)
+            # If there's no link found, continue to next iteration
+            if not link_element:
+                continue
 
-        # Extract content from <main> element
-        main_content_element = await page.query_selector("main")
-        main_content = await main_content_element.inner_text() if main_content_element else ""
+            link = await link_element.get_attribute("href")
+            await page.goto(link)
 
-        # Split the content to lines and get the middle part
-        lines = main_content.splitlines()
-        mid_idx = len(lines) // 2
-        middle_content = "\n".join(lines[mid_idx-5:mid_idx+5]) if len(lines) > 10 else main_content
+            # Get the content of the <main> element, and if not found, get the content of the <body> element
+            main_content_element = await page.query_selector("main") or await page.query_selector("body")
+            content = await main_content_element.inner_text() if main_content_element else ""
 
-        # Combine title and middle content
-        title = await page.title()
-        content = "\n" + middle_content
-
-        # Remove extra whitespaces
-        content = "\n".join([line.strip() for line in content.splitlines() if line.strip()])
-
-        # Use regex to remove unwanted patterns
-        patterns_to_remove = [
-            r"https?://\S+",  # URLs
-            r"\S+@\S+",  # Emails
-            r"\d{5}-\d{4}|\d{5}|\d{9}",  # ZIP codes
-            r"\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}",  # Phone numbers
-            r"\d{1,5}\s\w.\s(\b\w*\b\s){1,2}\w*\.",  # Address
-        ]
-
-        for pattern in patterns_to_remove:
-            content = re.sub(pattern, "", content)
+            if content:
+                break
 
         await browser.close()
-        
-        return content
+        return content if content else "No suitable content found. The application will not proceed."
+
+
+
+
+
+
+
+
+
+
     
-def generate_gpt2_response(seed_text, max_length=50):
+def generate_gpt2_response(seed_text, max_length=512):
     input_ids = gpt2_tokenizer.encode(seed_text, return_tensors="pt")
-    output = gpt2_model.generate(input_ids, max_length=max_length, num_return_sequences=1, temperature=1.2)
+    output = gpt2_model.generate(input_ids, max_length=max_length, num_return_sequences=1, temperature=1.5)
     generated_text = gpt2_tokenizer.decode(output[0], skip_special_tokens=True)
     
     # Check if the generated text is the same as the seed text
@@ -223,15 +221,20 @@ async def main():
     else:
         print(" Gematria: No valid letters found.")
         
-        
-
-    # Directory path
+     # Directory path
     dir_path = './tarot'
     images = os.listdir(dir_path)
+    
+    
+    # use total_value as tarot draw seed
+    secretsGenerator = secrets.SystemRandom()
+    secretsGenerator.seed(total_value)
 
     # Utilize CSPRNG
-    rng = random.SystemRandom()
-    selected_card = rng.choice(images)
+    # rng = random.SystemRandom()
+    # selected_card = rng.choice(images)
+    selected_card = secretsGenerator.choice(images)
+
 
     # Open and display the selected image
     image_path = os.path.join(dir_path, selected_card)
@@ -241,10 +244,32 @@ async def main():
     # Output the card name
     card_name = os.path.splitext(selected_card)[0]
     print('Card Name:', card_name)
+    
+    #sentiment score 
+    #sia = SentimentIntensityAnalyzer()
+    #sentiment_result = sia.polarity_scores(gematrix_result)
+    #sentiment_score = sentiment_result['compound']
+    #print(f"Gematria Sentiment Score: {sentiment_score}")
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+
+    # Tokenize the input
+    inputs = tokenizer(gematrix_result, return_tensors="pt", max_length=512, truncation=True)
+
+    # Predict the sentiment
+    outputs = model(**inputs)
+    probs = softmax(outputs.logits, dim=-1)
+
+    # Get the sentiment score; the result will depend on how the model was fine-tuned
+    sentiment_score = probs[0][1].item() - probs[0][0].item()
+
+    print(f"Sentiment Score: {sentiment_score}")
+
+
+
+await main()
+
+
 
 
 
